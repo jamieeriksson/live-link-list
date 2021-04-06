61ad20d1317f1cbcea723df17da142c41d713809
Jamie Eriksson <jamieeriksson@gmail.com>
2021-04-06 10:49:03 -0700

    Add initialize platforms command

---
+++ backend/livelinklist/lives/management/commands/initialize-platforms.py
@@ -0,0 +1,30 @@
+import json
+import random
+from pathlib import Path
+
+from django.core.management import call_command
+from django.core.management.base import BaseCommand
+from faker import Faker
+
+from livelinklist.lives.factories import LiveFactory
+from livelinklist.lives.models import Hashtag, Live, Platform
+from livelinklist.lives.tests.initialize_models import (
+    get_random_usernames,
+    get_string_hashtags,
+    initialize_platforms,
+)
+from livelinklist.users.factories import UserFactory
+from livelinklist.users.models import User
+
+fake = Faker()
+
+
+class Command(BaseCommand):
+    help = "Initializes database with platforms"
+
+    def handle(self, *args, **kwargs):
+        print("Seeding database...")
+
+        initialize_platforms()
+
+        print("Database initialized successfully")
