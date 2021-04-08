12c7cac63ba5da2e6f19fb5440f77bce11044606
Jamie Eriksson <jamieeriksson@gmail.com>
2021-04-08 13:49:51 -0700

    Fix search bar styling

--- frontend/src/browse-page/browse-page.js
+++ frontend/src/browse-page/browse-page.js
@@ -73,7 +73,7 @@ function SearchBar(props) {
   };
 
   return (
-    <div className="max-w-4xl w-full mt-4 mb-24 shadow-lg">
+    <div className="mt-4 mb-24 shadow-lg">
       <form onSubmit={handleSubmit} className="flex">
         <div className="hidden px-6 md:flex place-items-center bg-gray-800 rounded-l-md font-body font-semibold text-lg text-gray-200">
           Search
@@ -83,7 +83,7 @@ function SearchBar(props) {
           value={searchInput}
           onChange={(e) => setSearchInput(e.target.value)}
           placeholder="enter a hashtag or username"
-          className="px-4 py-2 flex place-items-center flex-grow bg-gray-50 border-t border-b border-l rounded-l-md border-gray-200 focus:outline-none"
+          className="px-2 md:px-4 py-1 md:py-2 flex place-items-center flex-grow bg-gray-50 border-t border-b border-l rounded-l-md md:rounded-none border-gray-200 focus:outline-none"
         />
 
         <div className="float-right font-body" ref={outsideClickRef}>
@@ -309,12 +309,14 @@ export default function BrowsePage() {
       <div
         className={`max-w-screen w-full min-h-screen h-full md:mt-5 px-1 pb-20 flex flex-col place-items-center font-body bg-gradient-to-t from-red-100 via-gray-50 to-gray-50`}
       >
-        <SearchBar
-          query={query}
-          searchInput={searchInput}
-          setSearchInput={setSearchInput}
-          setQuery={setQuery}
-        />
+        <div className="px-3 max-w-4xl w-full">
+          <SearchBar
+            query={query}
+            searchInput={searchInput}
+            setSearchInput={setSearchInput}
+            setQuery={setQuery}
+          />
+        </div>
         <p className="mb-16 px-4 text-body text-lg text-center font-semibold tracking-wide">
           So sorry, there was an error retrieving lives.
           <br />
@@ -327,7 +329,7 @@ export default function BrowsePage() {
     <div
       className={`max-w-screen w-full min-h-screen h-full md:mt-5 px-1 pb-20 flex flex-col place-items-center font-body bg-gradient-to-t from-red-100 via-gray-50 to-gray-50`}
     >
-      <div className="px-3">
+      <div className="px-3 max-w-4xl w-full">
         <SearchBar
           query={query}
           searchInput={searchInput}
